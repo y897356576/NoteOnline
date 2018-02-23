038e1775ecf81d749abd1179a15933758087eb9b
y897356576 <897356576@qq.com>
2018-02-23 15:12:07 +0800

    session会话失效时清除redis中的用户信息

--- src/main/java/com/stone/common/listener/HttpSessionListenerProcessor.java
+++ src/main/java/com/stone/common/listener/HttpSessionListenerProcessor.java
@@ -1,36 +1,51 @@
 package com.stone.common.listener;
 
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
+import com.stone.common.redis.RedisShard;
+import org.apache.commons.collections4.CollectionUtils;
+import redis.clients.jedis.Jedis;
+
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.Set;
 
 /**
  * Created by 石头 on 2017/6/20.
  */
-public class HttpSessionListenerTest implements HttpSessionListener, HttpSessionAttributeListener {
+public class HttpSessionListenerProcessor implements HttpSessionListener {
 
     //Session创建事件发生在每次一个新的session创建的时候，类似地Session失效事件发生在每次一个Session失效的时候。
 
     private Integer numMark = 0;
 
     @Override
-    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+    public void sessionCreated(HttpSessionEvent event) {
         numMark++;
-        System.out.println("SessionCreate;num:" + numMark + "; SessionId:" + httpSessionEvent.getSession().getId());
     }
 
     @Override
-    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+    public void sessionDestroyed(HttpSessionEvent event) {
         numMark--;
-        System.out.println("SessionDestory;num:" + numMark + "; SessionId:" + httpSessionEvent.getSession().getId());
+        this.removeRedisVal(event);
+    }
+
+    /**
+     * 会话失效后清除redis中用于标识用户登录的用户信息
+     * @param event
+     */
+    private void removeRedisVal(HttpSessionEvent event) {
+        String sessionId = event.getSession().getId();
+        Jedis jedis = RedisShard.getRedisNode(sessionId);
+        Set<String> columns = jedis.hkeys(sessionId + "_user");
+        if (CollectionUtils.isNotEmpty(columns)) {
+            jedis.hdel(sessionId + "_user", columns.toArray(new String[]{}));
+        }
     }
 
 
-
+    //继承 HttpSessionAttributeListener
     //session中属性的 创建、修改删除、属性被重新设置 时触发
 
-    @Override
+    /*@Override
     public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
         System.out.println("SessionAttribute_Add_SessionId:" + httpSessionBindingEvent.getSession().getId() +
                 "; thisNameValue:" + httpSessionBindingEvent.getName() + "_" + httpSessionBindingEvent.getValue());
@@ -45,5 +60,5 @@ public class HttpSessionListenerTest implements HttpSessionListener, HttpSession
     @Override
     public void attributeReplaced(HttpSessionBindingEvent httpSessionBindingEvent) {
         System.out.println("SessionAttribute_Replace_SessionId:" + httpSessionBindingEvent.getSession().getId());
-    }
+    }*/
 }
