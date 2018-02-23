eac195cbaac99d10a7efb4c00f67a90fcb7539ba
y897356576 <897356576@qq.com>
2018-02-23 15:13:18 +0800

    用户登录时将用户信息存入redis，用于登录过滤器处判断用户是否登录

--- src/main/java/com/stone/common/filter/LoginFilter.java
+++ src/main/java/com/stone/common/filter/LoginFilter.java
@@ -1,9 +1,15 @@
 package com.stone.common.filter;
 
+import com.stone.common.redis.RedisShard;
+import com.stone.common.util.ObjMapTransUtil;
+import com.stone.core.model.User;
+import redis.clients.jedis.Jedis;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by 石头 on 2017/6/20.
@@ -15,17 +21,24 @@ public class LoginFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         String path = request.getRequestURI();
+        String sessionId = request.getSession().getId();
 
-        Object user = request.getSession().getAttribute("login_user");
-        if(user == null && path.indexOf("/login.jsp")<0){
-            response.sendRedirect("/front/login.jsp");
-        } else if(user != null && path.indexOf("/login.jsp") >= 0) {
-            response.sendRedirect("/front/index.jsp");
+        User user = null;
+        Jedis jedis = RedisShard.getRedisNode(sessionId);
+        Map userMap = jedis.hgetAll(sessionId + "_user");
+        if (userMap != null && userMap.size() > 0) {
+            user = (User) ObjMapTransUtil.mapToObj(userMap, User.class);
+        }
+        if(user == null && !path.contains("/login.html")){
+            response.sendRedirect("/login.html");
+        } else if(user != null && path.contains("/login.html")) {
+            response.sendRedirect("/html/index.html");
         } else {
             //将请求转发给过滤器链上下一个对象,没有下一个filter就转向请求的资源
             //System.out.println("请求执行前")
